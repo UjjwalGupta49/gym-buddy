bcbc170eacd87a923fff6f81e4b5f4d788f33782
Arjav014 <arjav2768@gmail.com>
2024-08-11 19:09:27 +0530

    update: footer added

--- frontend/components/index.tsx
+++ frontend/components/index.tsx
@@ -1,4 +1,5 @@
 import Hero from "./Hero";
 import WorkoutView from "./workout/WorkoutView";
+import Footer from "./Footer"
 
-export {Hero, WorkoutView};
+export {Hero, WorkoutView, Footer};
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,11 @@
-import { Hero, WorkoutView } from "@/components";
+import { Hero, WorkoutView, Footer } from "@/components";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <WorkoutView />
+      <Footer />
     </div>
   );
 }
